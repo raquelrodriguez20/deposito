3310463e97faf91b9e4ed3d4f7b2084d817e6793
raquel <raquel@raquel>
2022-02-20 02:19:12 +0100

    Commit deposito

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -14,21 +14,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        setNombre(nom);
-        setCuenta(cue);
-        setSaldo(sal);
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
     }
 
     public double estado()
     {
-        return getSaldo();
+        return saldo;
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        setSaldo(getSaldo() + cantidad);
+        saldo = saldo + cantidad;
     }
 
     public void retirar(double cantidad) throws Exception
@@ -37,38 +37,6 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        setSaldo(getSaldo() - cantidad);
+        saldo = saldo - cantidad;
     }
-
-	private String getNombre() {
-		return nombre;
-	}
-
-	private void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	private String getCuenta() {
-		return cuenta;
-	}
-
-	private void setCuenta(String cuenta) {
-		this.cuenta = cuenta;
-	}
-
-	private double getSaldo() {
-		return saldo;
-	}
-
-	private void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-
-	private double getTipoInteres() {
-		return tipoInteres;
-	}
-
-	private void setTipoInteres(double tipoInteres) {
-		this.tipoInteres = tipoInteres;
-	}
 }
